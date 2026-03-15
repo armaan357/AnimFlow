b17d26dd91084f8525880152b1715481ac7464a0
armaan <armaanmishra13@gmail.com>
2026-03-15 16:56:02 +0530

    changed_authPages

--- apps/web/app/(auth)/signin/page.tsx
+++ apps/web/app/(auth)/signin/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Button } from "@repo/ui/button";
-import InputBox from "@repo/ui/input";
+import InputBox, { PasswordInputBox } from "@repo/ui/input";
 import axios from "axios";
 import { Dispatch, RefObject, SetStateAction, useRef, useState } from "react";
 import { useRouter } from "next/navigation";
@@ -10,7 +10,7 @@ const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 const checkEmailExists = async (
 	emailRef: RefObject<HTMLInputElement | null>,
-	setPasswordVisible: Dispatch<SetStateAction<boolean>>,
+	setPasswordInputVisible: Dispatch<SetStateAction<boolean>>,
 	setErrorMsgVisible: Dispatch<SetStateAction<boolean>>,
 ) => {
 	if (!backendUrl) {
@@ -31,7 +31,7 @@ const checkEmailExists = async (
 		}
 
 		if (resp.data.exists) {
-			setPasswordVisible(true);
+			setPasswordInputVisible(true);
 		} else {
 			setErrorMsgVisible(true);
 		}
@@ -41,8 +41,10 @@ const checkEmailExists = async (
 };
 
 export default function Signin() {
-	const [isPasswordVisible, setPasswordVisible] = useState<boolean>(false);
+	const [isPasswordInputVisible, setPasswordInputVisible] =
+		useState<boolean>(false);
 	const [isErrorMsgVisible, setErrorMsgVisible] = useState<boolean>(false);
+	const [isPasswordVisible, setPasswordVisible] = useState<boolean>(false);
 	const emailRef = useRef<HTMLInputElement | null>(null);
 	const passwordRef = useRef<HTMLInputElement | null>(null);
 	const navigate = useRouter();
@@ -69,22 +71,42 @@ export default function Signin() {
 	};
 
 	return (
-		<div className="grid h-full w-dvw min-h-screen lg:grid-cols-2 bg-[#181818]">
+		<div className="flex flex-col justify-center items-center h-full w-full min-h-screen overflow-hidden bg-black">
+			<div className="h-15 absolute top-0 w-full flex justify-between items-center px-3 sm:px-8 md:px-12">
+				<div>
+					<Link href={"/"} className="cursor-pointer">
+						<div className="w-fit cursor-pointer">
+							<span className="font-semibold text-xl text-[#e3e3e3]">
+								Anim
+								<span className="text-[#488AED]/75">Flow</span>
+							</span>
+						</div>
+					</Link>
+				</div>
+				<div>
+					<Button
+						variant="secondary"
+						size="md"
+						children={"Sign Up"}
+						onClick={() => navigate.push("/signup")}
+					/>
+				</div>
+			</div>
 			<div className="flex justify-center items-center px-4">
-				<div className="relative flex flex-col justify-center max-w-87.5 w-full">
+				<div className="relative flex flex-col justify-center max-w-87 w-87 sm:max-w-4xl sm:w-92 md:w-97.5">
 					<div className="w-full pt-5 pb-10">
-						<div className="w-auto flex justify-start">
-							<div className="w-fit">
+						<div className="w-auto flex justify-center">
+							{/* <div className="w-fit">
 								<span className="font-bold text-2xl text-[#e3e3e3]">
 									AnimFlow
 								</span>
-							</div>
+							</div> */}
 						</div>
-						<div className="text-2xl font-bold text-[#ffffff80]">
+						<div className="text-2xl text-center font-bold text-[#ffffff80]">
 							Log in to your account
 						</div>
 					</div>
-					<div className="w-full flex flex-col gap-4 justify-center items-center">
+					<div className="w-full flex flex-col gap-4 px-7 sm:px-3 md:px-0.5 justify-center items-center">
 						<div className="w-full">
 							<Button
 								variant="secondary"
@@ -111,15 +133,15 @@ export default function Signin() {
 						</div>
 						<div className="relative w-full">
 							<div className="absolute flex items-center inset-0 w-full">
-								<span className="w-full border-t border-[#ffffff15]" />
+								<span className="w-full border-t border-[#ffffff25]" />
 							</div>
 							<div className="relative flex justify-center text-xs uppercase">
-								<span className="px-2 bg-[#1a1a1a] text-[#ffffff85]">
+								<span className="px-2 bg-black text-[#ffffff85]">
 									Or
 								</span>
 							</div>
 						</div>
-						<div className="flex flex-col gap-3 w-full">
+						<div className="flex flex-col justify-center text-center gap-3 w-full">
 							<InputBox
 								id="email"
 								placeholder="Email"
@@ -127,12 +149,15 @@ export default function Signin() {
 								type="email"
 								ref={emailRef}
 							/>
-							{isPasswordVisible && (
-								<InputBox
-									id="password"
+							{isPasswordInputVisible && (
+								<PasswordInputBox
+									id="pwd"
 									placeholder="Password"
-									type="password"
 									ref={passwordRef}
+									isPasswordVisible={isPasswordVisible}
+									setPasswordVisible={() =>
+										setPasswordVisible(!isPasswordVisible)
+									}
 								/>
 							)}
 							{isErrorMsgVisible && (
@@ -153,18 +178,21 @@ export default function Signin() {
 								size="full"
 								disabled={isErrorMsgVisible ? true : false}
 								onClick={() => {
-									if (isPasswordVisible && !isErrorMsgVisible)
+									if (
+										isPasswordInputVisible &&
+										!isErrorMsgVisible
+									)
 										signin();
 									else
 										checkEmailExists(
 											emailRef,
-											setPasswordVisible,
+											setPasswordInputVisible,
 											setErrorMsgVisible,
 										);
 								}}
 							/>
-							<div
-								className={`flex justify-center mt-4 ${isErrorMsgVisible ? " invisible " : ""}`}
+							{/* <div
+								className={`flex justify-center text-center mt-4 ${isErrorMsgVisible ? " invisible " : ""}`}
 							>
 								<p className="text-white/80 text-base ">
 									Don't have an account?{" "}
@@ -172,22 +200,21 @@ export default function Signin() {
 										href={"/signin"}
 										className={`text-blue-300 transition-all duration-150 ease-in-out hover:text-blue-500 hover:underline cursor-pointer`}
 									>
-										Create
+										Sign Up
 									</Link>{" "}
-									a new account.
 								</p>
-							</div>
+							</div> */}
 						</div>
 					</div>
 				</div>
 			</div>
-			<div className="sticky top-0 hidden lg:block h-screen rounded-xl p-4">
+			{/* <div className="sticky top-0 hidden lg:block h-screen rounded-xl p-4">
 				<div className="bg-black w-full h-full rounded-lg flex justify-center items-center">
 					<p className="text-4xl text-[#ffffff80] font-bold">
 						A Play button logo will appear here!
 					</p>
 				</div>
-			</div>
+			</div> */}
 		</div>
 	);
 }
--- apps/web/app/(auth)/signup/page.tsx
+++ apps/web/app/(auth)/signup/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Button } from "@repo/ui/button";
-import InputBox from "@repo/ui/input";
+import InputBox, { PasswordInputBox } from "@repo/ui/input";
 import axios from "axios";
 import { Dispatch, RefObject, SetStateAction, useRef, useState } from "react";
 import { useRouter } from "next/navigation";
@@ -10,7 +10,7 @@ const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 const checkEmailExists = async (
 	emailRef: RefObject<HTMLInputElement | null>,
-	setPasswordVisible: Dispatch<SetStateAction<boolean>>,
+	setPasswordInputVisible: Dispatch<SetStateAction<boolean>>,
 	setErrorMsgVisible: Dispatch<SetStateAction<boolean>>,
 ) => {
 	console.log("function started!!\n\n\n");
@@ -34,7 +34,7 @@ const checkEmailExists = async (
 		if (resp.data.exists) {
 			setErrorMsgVisible(true);
 		} else {
-			setPasswordVisible(true);
+			setPasswordInputVisible(true);
 			console.log(resp.data.error);
 		}
 	} catch (e: any) {
@@ -56,25 +56,36 @@ const loginWithGoogle = async () => {
 };
 
 export default function SignUp() {
-	const [isPasswordVisible, setPasswordVisible] = useState<boolean>(false);
+	const [isPasswordInputVisible, setPasswordInputVisible] =
+		useState<boolean>(false);
 	const [isErrorMsgVisible, setErrorMsgVisible] = useState<boolean>(false);
+	const [isPasswordVisible, setPasswordVisible] = useState<boolean>(false);
+	const [isCnfPasswordVisible, setCnfPasswordVisible] =
+		useState<boolean>(false);
 	const emailRef = useRef<HTMLInputElement | null>(null);
 	const passwordRef = useRef<HTMLInputElement | null>(null);
+	const cnfPasswordRef = useRef<HTMLInputElement>(null);
 	const userNameRef = useRef<HTMLInputElement | null>(null);
 	const navigate = useRouter();
 
 	const signup = async () => {
-		const userInfo: { email: string; password: string; userName: string } =
-			{
-				email: emailRef.current?.value!,
-				password: passwordRef.current?.value!,
-				userName: userNameRef.current?.value!,
-			};
+		const userInfo: {
+			email: string;
+			password: string;
+			userName: string;
+			cnfPassword: string;
+		} = {
+			email: emailRef.current?.value!,
+			userName: userNameRef.current?.value!,
+			password: passwordRef.current?.value!,
+			cnfPassword: cnfPasswordRef.current?.value!,
+		};
 		try {
 			if (
 				userInfo.email.length === 0 ||
 				userInfo.password.length === 0 ||
-				userInfo.userName.length === 0
+				userInfo.userName.length === 0 ||
+				userInfo.cnfPassword.length === 0
 			)
 				return;
 			const signupResp = await axios.post(
@@ -89,22 +100,42 @@ export default function SignUp() {
 	};
 
 	return (
-		<div className="grid h-full w-dvw min-h-screen lg:grid-cols-2 bg-[#181818]">
+		<div className="flex flex-col justify-center items-center h-full w-full min-h-screen bg-black">
+			<div className="h-15 absolute top-0 w-full flex justify-between items-center px-3 sm:px-8 md:px-12">
+				<div>
+					<Link href={"/"} className="cursor-pointer">
+						<div className="w-fit cursor-pointer">
+							<span className="font-semibold text-xl text-[#e3e3e3]">
+								Anim
+								<span className="text-[#488AED]/75">Flow</span>
+							</span>
+						</div>
+					</Link>
+				</div>
+				<div>
+					<Button
+						variant="secondary"
+						size="md"
+						children={"Log In"}
+						onClick={() => navigate.push("/signin")}
+					/>
+				</div>
+			</div>
 			<div className="flex justify-center items-center px-4">
-				<div className="relative flex flex-col justify-center max-w-87.5 w-full">
+				<div className="relative flex flex-col justify-center max-w-87 w-87 sm:max-w-4xl sm:w-92 md:w-97.5">
 					<div className="w-full pt-5 pb-10">
 						<div className="w-auto flex justify-start">
-							<div className="w-fit">
+							{/* <div className="w-fit">
 								<span className="font-bold text-2xl text-[#e3e3e3]">
 									AnimFlow
 								</span>
-							</div>
+							</div> */}
 						</div>
-						<div className="text-2xl font-bold text-[#ffffff80]">
+						<div className="text-2xl font-bold text-center text-[#ffffff80]">
 							Create Your Account
 						</div>
 					</div>
-					<div className="w-full flex flex-col gap-4 justify-center items-center">
+					<div className="w-full flex flex-col gap-4 justify-center text-center items-center">
 						<div className="w-full">
 							<Button
 								variant="secondary"
@@ -129,15 +160,15 @@ export default function SignUp() {
 						</div>
 						<div className="relative w-full">
 							<div className="absolute flex items-center inset-0 w-full">
-								<span className="w-full border-t border-[#ffffff15]" />
+								<span className="w-full border-t border-[#ffffff25]" />
 							</div>
 							<div className="relative flex justify-center text-xs uppercase">
-								<span className="px-2 bg-[#1a1a1a] text-[#ffffff85]">
+								<span className="px-2 bg-black text-[#ffffff85]">
 									Or
 								</span>
 							</div>
 						</div>
-						<div className="flex flex-col gap-4 w-full">
+						<div className="flex flex-col text-center gap-4 w-full">
 							<InputBox
 								id="email"
 								placeholder="Email"
@@ -145,19 +176,35 @@ export default function SignUp() {
 								type="email"
 								ref={emailRef}
 							/>
-							{isPasswordVisible && (
+							{isPasswordInputVisible && (
 								<div className="flex flex-col gap-4 w-full">
-									<InputBox
-										id="password"
-										placeholder="Password"
-										type="password"
-										ref={passwordRef}
-									/>
 									<InputBox
 										id="userName"
 										placeholder="Enter your name"
 										type="text"
 										ref={userNameRef}
+									/>
+									<PasswordInputBox
+										id="pwd"
+										placeholder="Password"
+										ref={passwordRef}
+										isPasswordVisible={isPasswordVisible}
+										setPasswordVisible={() =>
+											setPasswordVisible(
+												!isPasswordVisible,
+											)
+										}
+									/>
+									<PasswordInputBox
+										id="cnf-pwd"
+										placeholder="Confirm Password"
+										ref={cnfPasswordRef}
+										isPasswordVisible={isCnfPasswordVisible}
+										setPasswordVisible={() =>
+											setCnfPasswordVisible(
+												!isCnfPasswordVisible,
+											)
+										}
 									/>
 								</div>
 							)}
@@ -179,17 +226,20 @@ export default function SignUp() {
 								size="full"
 								disabled={isErrorMsgVisible ? true : false}
 								onClick={() => {
-									if (isPasswordVisible && !isErrorMsgVisible)
+									if (
+										isPasswordInputVisible &&
+										!isErrorMsgVisible
+									)
 										signup();
 									else
 										checkEmailExists(
 											emailRef,
-											setPasswordVisible,
+											setPasswordInputVisible,
 											setErrorMsgVisible,
 										);
 								}}
 							/>
-							<div
+							{/* <div
 								className={`flex justify-center mt-4 ${isErrorMsgVisible ? " invisible " : ""}`}
 							>
 								<p className="text-white/80 text-base">
@@ -202,18 +252,18 @@ export default function SignUp() {
 									</Link>{" "}
 									to your account.
 								</p>
-							</div>
+							</div> */}
 						</div>
 					</div>
 				</div>
 			</div>
-			<div className="sticky top-0 hidden lg:block h-screen rounded-xl p-4">
+			{/* <div className="sticky top-0 hidden lg:block h-screen rounded-xl p-4">
 				<div className="bg-black w-full h-full rounded-lg flex justify-center items-center">
 					<p className="text-4xl text-[#ffffff80] font-bold">
 						A Play button logo will appear here!
 					</p>
 				</div>
-			</div>
+			</div> */}
 		</div>
 	);
 }
