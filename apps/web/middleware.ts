c3ce608bd7f43e18964dc0cdf32a0352e88f7f3c
armaan <armaanmishra13@gmail.com>
2026-03-22 03:05:49 +0530

    middleware_changed

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -9,9 +9,9 @@ const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
 export async function middleware(req: NextRequest) {
 	const sessionCookie = req.cookies.get("connect.sid")?.value;
 
-	if (!sessionCookie) {
-		return NextResponse.redirect(new URL("/signin", req.url));
-	}
+	// if (!sessionCookie) {
+	// 	return NextResponse.redirect(new URL("/signin", req.url));
+	// }
 	try {
 		const c = await cookies();
 		const response = await axios.get(`${backendUrl}user/auth-me`, {
