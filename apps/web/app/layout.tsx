1bc6384eb0947bb26f9d69dcd63ba3f991a3b3a1
armaan <armaanmishra13@gmail.com>
2026-03-28 03:33:46 +0530

    changed_favicon

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -20,6 +20,12 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
+			<head>
+				<link rel="icon" type="image/svg+xml" href="./favicon.svg" />
+				<link rel="icon" type="image/png" href="./favicon.png" />
+				<link rel="icon" href="./favicon.ico" />
+			</head>
+
 			<body className={`${inter.className}`}>{children}</body>
 		</html>
 	);
